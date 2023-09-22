3126b7ed9c5c38a09e65a292bb223f0210f943ef
Chromadesk <yisraeleliyah@gmail.com>
2023-09-22 18:22:47 -0400

    Animation Loading

--- src/shared/Classes/Entities/Enemy/Enemy.lua
+++ src/shared/Classes/Entities/Enemy/Enemy.lua
@@ -1,13 +1,16 @@
 local Class = require(game.ReplicatedStorage.Classes.Class)
 local Enemy = Class:Extend()
 local AiBehaviorClass = require(game.ReplicatedStorage.Classes.Entities.Enemy.AiBehavior)
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 Enemy.name = nil
 Enemy.maxHealth = nil
 Enemy.maxSpeed = nil
 Enemy.model = nil
+Enemy.animator = nil
 Enemy.humanoid = nil
 Enemy.AiBehavior = nil
+Enemy.animations = nil
 
 function Enemy:OnNew()
     assert(self.name, "Enemy must have a name.")
@@ -17,9 +20,10 @@ function Enemy:OnNew()
 
     self.assetFolder = self.assetFolder:Clone()
     self.model = self.assetFolder[self.name]
+    self.humanoid = self.model.Humanoid
+    self.animator = self.humanoid.Animator
     self.maxHealth = self.assetFolder.Stats.Health.Value
     self.maxSpeed = self.assetFolder.Stats.Speed.Value
-    self.humanoid = self.model.Humanoid
 
     self.humanoid.Died:Connect(
         function()
@@ -40,8 +44,35 @@ end
 
 function Enemy:Spawn(spawnPos)
     self.model.Parent = workspace.NPCs
+    self:LoadAnimations()
     self.model:MoveTo(spawnPos)
     self.AiBehavior = AiBehaviorClass:New({NPC = self})
+end
+
+function Enemy:LoadAnimations()
+    self.animations = {}
+    game:GetService("RunService").Stepped:Wait()
+    self.animations["move"] = self.animator:LoadAnimation(ReplicatedStorage.Assets.Animations.Move)
+    self.animations["idle"] = self.animator:LoadAnimation(ReplicatedStorage.Assets.Animations.Idle)
+    self.animations["attack"] = self.animator:LoadAnimation(ReplicatedStorage.Assets.Animations.Attack)
+    self.animations["guard"] = self.animator:LoadAnimation(ReplicatedStorage.Assets.Animations.Guard)
+
+    self.humanoid.Running:Connect(
+        function(movementSpeed)
+            if movementSpeed > 0 then
+                if not self.animations.move.IsPlaying then
+                    self.animations.idle:Stop()
+                    self.animations.move:Play()
+                end
+            else
+                if self.animations.move.IsPlaying then
+                    self.animations.move:Stop()
+                    self.animations.idle:Play()
+                end
+            end
+        end
+    )
+    self.animations.idle:Play()
 end
 
 return Enemy
